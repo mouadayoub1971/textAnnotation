94b85b2c512e862d2721969da84d1606eee7ff25
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-05-10 22:33:00 +0100

    first commit

---
+++ src/test/java/com/mouad/textannotation/TextAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouad.textannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
